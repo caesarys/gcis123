cdef8146503a6696949bf27d1531c2063c4ad8a9
caesarys <146090528+caesarys@users.noreply.github.com>
2024-05-02 23:35:57 +0400

    Add files via upload

---
+++ display.py
@@ -0,0 +1,8 @@
+def menu():
+    print("Menu:")
+    print("1. List all the items, inventory, and price")
+    print("2. Add an item to the shopping cart")
+    print("3. Remove an item from the shopping cart")
+    print("4. List what is inside Cart")
+    print("5. Checkout")
+    print("6. Exit")
